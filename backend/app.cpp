1456a8bbc6f2c9a233a85ea35a14c3c2ba03284e
Nirmalsai <138203410+Nirmalsai@users.noreply.github.com>
2026-03-01 10:41:46 +0530

    changed app

--- backend/app.cpp
+++ backend/app.cpp
@@ -41,14 +41,17 @@ int main() {
     while(true) {
         int client_fd = accept(server_fd, NULL, NULL);
         if (client_fd < 0) continue;
-        
-        // Simple HTTP response
-        std::string response = "HTTP/1.1 200 OK\r\n";
-        response += "Content-Type: text/plain\r\n";
-        response += "Connection: close\r\n\r\n";
-        response += "Served by backend: " + std::string(hostname) + "\n";
-        
-        send(client_fd, response.c_str(), response.length(), 0);
+        std::string body = "Served by backend: " + std::string(hostname) + "\n";
+
+        std::string response =
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n"
+            "Content-Length: " + std::to_string(body.size()) + "\r\n"
+            "Connection: close\r\n"
+            "\r\n" +
+            body;
+
+        send(client_fd, response.c_str(), response.size(), 0);
         close(client_fd);
     }
     
